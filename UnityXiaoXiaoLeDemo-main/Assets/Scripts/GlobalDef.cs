ea10f2aeed194dd776cbf095f0225c597bd8ad26
mukuxie <57396715+mukuxie@users.noreply.github.com>
2022-05-08 09:18:16 +0800

    專題

---
+++ UnityXiaoXiaoLeDemo-main/Assets/Scripts/GlobalDef.cs
@@ -0,0 +1,16 @@
+﻿
+public class GlobalDef 
+{
+    /// <summary>
+    /// 行数
+    /// </summary>
+    public const int ROW_COUNT = 5;
+    /// <summary>
+    /// 列数
+    /// </summary>
+    public const int COLUM_COUNT = 6;  
+    /// <summary>
+    /// 每个格子大小
+    /// </summary>
+    public const float CELL_SIZE = 0.9f;
+}
